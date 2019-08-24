2ea0daa0089d8039b56a61c87782872d2a47bb4e
omerakgoz34 <49201485+omerakgoz34@users.noreply.github.com>
2019-08-24 15:05:56 +0300

    Add files via upload

--- ytdl.sh
+++ ytdl.sh
@@ -1,88 +1,101 @@
-#!/bin/bash
+#!/bin/sh
 
-# youtube-dl Video and Audio downloader script.
+# youtube-dl downloader script.
 # Written by QuidsUp
 # Edited by Christoph Korn
-# Much much more edited by omerakgoz34 =)
+# Much much more edited by omerakgoz34 :)
 
 # v1.1 - 8 May 2019
 
 URL=$1
-VAR=0
 
 # Help message
 if [ -z $URL ]; then
-	echo
-	echo "Usage: ytdl <URL>"
-	echo
+	echo ""
+	echo "Usage: ./ytdl.sh <URL>"
+	echo ""
 	echo "Dependencies: Python, youtube-dl, FFmpeg."
-	echo
+	echo ""
+	echo "This Script created by omerakgoz34"
+	echo "Credits: omerakgoz34, Christoph Korn, QuidsUp"
+	echo ""
 	exit
 fi
 
-DownType() {
-    echo
-    echo "1 = video"
-    echo "2 = audio"
-    echo "3 = video only"
-    echo -n "Enter download type [1/2/3]: "
-    read DownType
-    echo
-}
+echo ""
+echo "1 = video + audio"
+echo "2 = video only"
+echo "3 = audio"
+echo -n "Enter download type [1/2/3]: "
+read DownType
+echo ""
 
-if [ $VAR = 0 ]; then
-    VAR = 1
-    DownType
-fi
+if [ $DownType == "1" ]; then
+    # video + audio
 
-if [ $DownType = "1" ]; then
-    # video
-    
     youtube-dl -F $URL
-    echo
-    
+    echo ""
+
     echo -n "Select quality for Video [default 136]: "
     read DownOptionV
     if [ -z $DownOptionV ]; then
-        DownOptionV=136
+        DownOptionV = 136
     fi
-    
+
     echo -n "Select quality for Audio [default 140]: "
     read DownOptionA
     if [ -z $DownOptionA ]; then
-        DownOptionA=140
-		  fi
-		  echo
-		  FileV=$(youtube-dl --get-filename -f $DownOptionV $URL)
-		  FileA=$(youtube-dl --get-filename -f $DownOptionA $URL)
-		  Out=${FileV:0:${#FileV}-16}
-		  echo "File name: " $Out
-		  
-		  if [ $DownOptionV = "278" ]; then
-		      echo done
-		      exit
-		  fi
+        DownOptionA = 140
+	fi
+	echo ""
+	FileV = $(youtube-dl --get-filename -f $DownOptionV $URL)
+	FileA = $(youtube-dl --get-filename -f $DownOptionA $URL)
+	Out = ${FileV:0:${#FileV}-16}
+
+	youtube-dl -f $DownOptionV $URL
+	if [ -f $FileV ]; then
+		echo ""
+        echo "* Video Download Failed."
+		exit
+	fi
+
+	youtube-dl -f $DownOptionA $URL
+	if [ -f $FileA ]; then
+		echo ""
+        echo "* Audio Download Failed."
+		exit
+	fi
+
+	ffmpeg -i $FileV -i $FileA -threads 0 -c:v copy -c:a copy $Out
+	if [ -f $Out ]; then
+		echo ""
+        echo "* Merging Failed."
+		exit
+	fi
+
+	echo $Out "created."
+	rm -f $FileV
+	rm -f $FileA
 else
 
-if [ $DownType = "2" ]; then
-    # audio
-    
-    echo akal
-else
-
-if [ $DownType = "3" ]; then
+if [ $DownType == "2" ]; then
     # video only
-    
-    echo skak
+    echo "asd"
 else
-    echo
+
+if [ $DownType == "3" ]; then
+    # audio
+    echo "asd"
+else
+    echo ""
     echo "* Wrong option!"
-    echo
+    echo ""
+	exit
 fi
 fi
 fi
 
 
 
-# Finish
+# Done.
 exit
